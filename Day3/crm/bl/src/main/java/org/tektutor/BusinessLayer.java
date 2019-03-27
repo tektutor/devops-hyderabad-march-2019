04be8596c405d857d9ecf47e5bfe990d396c13eb
Jeganathan Swaminathan <mail2jegan@gmail.com>
2019-03-27 11:35:30 +0530

    Fixed build issue.

--- Day3/crm/bl/src/main/java/org/tektutor/BusinessLayer.java
+++ Day3/crm/bl/src/main/java/org/tektutor/BusinessLayer.java
@@ -4,7 +4,7 @@ public class BusinessLayer {
 
 	public String printModuleName() {
 		System.out.println ( "BusinessLayer module" );
-		return "BusinessLayer module"
+		return "BusinessLayer module";
 	}
 
 }
